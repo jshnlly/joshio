175a981914a1f801b0d0817974cd0ea5c2abb033
Josh Nelson <jshnelson4@gmail.com>
2022-03-26 10:37:04 -0700

    .link

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <div className={styles.linkContainer}>
         <p className={styles.base}>
           <span className={styles.textLink}><a className={styles.link} href="https://read.cv/josh" target="_blank" rel="noreferrer">CV</a></span> • 
-          <span className={styles.textLink}><a className={styles.link} href="https://twitter.com/jnelly2" target="_blank" rel="noreferrer">Twitter</a></span> • <span className={styles.textLink}><a href="https://linkedin.com/in/jshn" target="_blank" rel="noreferrer">LinkedIn</a></span> • <span className={styles.textLink}><a className={styles.link} href="mailto:josh@joshn.io" target="_blank" rel="noreferrer">Email</a></span>
+          <span className={styles.textLink}><a className={styles.link} href="https://twitter.com/jnelly2" target="_blank" rel="noreferrer">Twitter</a></span> • <span className={styles.textLink}><a className={styles.link} href="https://linkedin.com/in/jshn" target="_blank" rel="noreferrer">LinkedIn</a></span> • <span className={styles.textLink}><a className={styles.link} href="mailto:josh@joshn.io" target="_blank" rel="noreferrer">Email</a></span>
           </p>
       </div>
 
